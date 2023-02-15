b926a31043017da33d6f4a7daf72108218653c0d
kanchal vinod <68420824+kanchal-vinod@users.noreply.github.com>
2023-02-15 13:56:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
-const app = express();
-
-app.use(bodyParser.json());
-
-app.post('/api/data', (req, res) => {
-  const data = req.body;
-  console.log(data);
-  res.send(data);
+ var port = 9000; app.post('/validate', function(req, res) {
+  var username=req.body.username;
+  var email=req.body.email;
+  if(username==email)
+{
+    console.log("True");
+    res.send(null);
+}
+else
+{
+    res.send(username);
+}
 });
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/',function(req,res){
+res.send("Hello from server");
 });
