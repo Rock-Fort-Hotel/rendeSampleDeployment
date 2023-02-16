c9025b89176e2768ca557d4767af2ca492e97ea6
kanchal vinod <68420824+kanchal-vinod@users.noreply.github.com>
2023-02-16 12:03:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/api/data', (req, res) => {
-  const data ={
+  const data =[{
     "row_id": 2,
     "column": "user_id",
-    "severity": "error",
+    "severity": "warning",
     "message": "Employee no longer works for company."
-  };
+  }];
   //res.send(data);
   // res.status(200).json(data);
   
