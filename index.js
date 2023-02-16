0a57e556c7d84c582405ed9e266aba9366a0f003
kanchal vinod <68420824+kanchal-vinod@users.noreply.github.com>
2023-02-16 11:13:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ app.post('/api/data', (req, res) => {
   const data ={
     "row_id": 2,
     "column": "user_id",
-    "severity": "warning",
+    "severity": "error",
     "message": "Employee no longer works for company."
   };
-  res.send(data);
+  //res.send(data);
   // res.status(200).json(data);
-  console.log(data);
+  const jsonData = JSON.stringify(data);
+  res.send(jsonData);
+  console.log(jsonData);
 });
 
 app.listen(9000, () => {
   console.log('Server listening on port 9000');
 });
 
-
+console.log(jsonData);
