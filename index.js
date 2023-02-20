8dc9680967d9b347850dc83af57019110de5b1e3
kanchal vinod <68420824+kanchal-vinod@users.noreply.github.com>
2023-02-20 11:20:29 +0530

    authentication added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
+const jsonwebtoken = require("jsonwebtoken");
 
 const app = express();
 
 app.use(bodyParser.json());
 
+
+
+// The secret should be an unguessable long string (you can use a password generator for this!)
+const JWT_SECRET =
+  "goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu";
+
+
+
+  //sample data from the oneschema format
 app.post('/api/data', (req, res) => {
   const data =[{
     "row_id": 2,
@@ -12,15 +23,40 @@ app.post('/api/data', (req, res) => {
     "severity": "warning",
     "message": "Employee no longer works for company."
   }];
-  //res.send(data);
-  // res.status(200).json(data);
   
+
   res.send(JSON.stringify(data));
   
 });
 
+//login beyond cloud resources
+app.get("/super-secure-resource", (req, res) => {
+  res
+    .status(401)
+    .json({ message: "You need to be logged in to access this resource" });
+});
+
+
+//2.Return a JWT token on successful login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log(`${username} is trying to login ..`);
+
+  if (username === "admin" && password === "admin") {
+    return res.json({
+      token: jsonwebtoken.sign({ user: "admin" }, JWT_SECRET),
+    });
+  }
+
+  return res
+    .status(401)
+    .json({ message: "The username and password your provided are invalid" });
+});
+
+
+
 app.listen(9000, () => {
   console.log('Server listening on port 9000');
 });
-
+ 
 
